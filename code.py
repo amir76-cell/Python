e77ba59fcf4a7b602f9ac58cc7d349918cb21a14
Amir <amir.07hossein18@gmail.com>
2026-04-16 15:15:54 +0200

    j'ai déja réalisé jusqu'à 11

---
+++ code.py
@@ -0,0 +1,81 @@
+"exercice 1"
+
+a = 1
+b = "FRANCE"
+c = 36.2
+
+print(a, b, c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+# exercice 2
+
+ch = "salut"
+ch = "ca va"
+print(ch)
+
+# exercice 3
+x = str(3)
+y = str(8.5)
+print(type(x))
+print(type(y))
+
+# exercice 4
+poids = int(input("Quel est votre poids en kg ? "))
+print(f"Le poids de l'utilisateur est {poids} kg")
+
+# exercice 5
+var = "bonjour"
+
+if type(var) == str:
+    print("c'est une chaine de caracter")
+
+elif type(var) == int:
+    print("c'est un entier")
+
+# exercice 6
+d = int(5)
+
+if d >= 0:
+    print("positiv")
+else:
+    print("negativ")
+
+# exercice 7
+age = int(input("Quel age avez vous ?"))
+
+if age >= 18:
+    print("L'utilisateur est majeur")
+else:
+    print("L'utilisateur est mineur")
+
+# exercice 8
+for i in range(1, 21):
+    print(i)
+
+
+compteur = 1
+while compteur <= 20:
+    print(compteur)
+    compteur += 1
+
+# exercice 9
+for i in range(10, 21):
+    if i % 2 == 0:
+        print(i)
+
+compteur = 10
+while compteur <= 20:
+    if compteur % 2 == 0:
+        print(compteur)
+    compteur += 1
+
+# exercice 10
+L = [i for i in range(1, 11)]
+print(L)
+
+# exercice 11
+L = [i for i in range(1, 11) if i % 2 == 0]
+print(L)
